1f7c76a038af64f4c3c750b65848ddbee392224e
bhuvandcs <bhuvan@dcstechnosis.com>
2026-05-01 15:56:31 +0530

    code push

---
+++ Controllers/RemunerationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NIAUNIVERSITYPANELAPI.Models;
+using NIAUNIVERSITYPANELAPI.Service;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RemunerationController : ControllerBase
+{
+    private readonly RemunerationService _service;
+    public RemunerationController(RemunerationService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("GetBill")]
+    public async Task<IActionResult> GetBill()
+    {
+        var data = await _service.GetBillAsync();
+
+        if (data == null || data.Count == 0)
+            return NotFound("No data found");
+
+        return Ok(data);
+    }
+}
---
+++ Models/RemunerationVM.cs
@@ -0,0 +1,21 @@
+﻿namespace NIAUNIVERSITYPANELAPI.Models
+{
+    public class RemunerationVM
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string PanNo { get; set; }
+
+        public string BankName { get; set; }
+        public string BankAccount { get; set; }
+        public string IFSCCode { get; set; }
+
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+
+        public int Count { get; set; }
+        public decimal Rate { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
